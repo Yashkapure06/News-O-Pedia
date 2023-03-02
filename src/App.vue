f0e545e2f95bec59e4899c1224fa8820c1215764
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2023-03-02 20:02:26 +0530

    Added NavBar in App.vue as an Main Component🚀

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <NavBar />
+
+  </div>
+</template>
+
+<script>
+import NavBar from './components/components/NavBar.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+  }
+}
+</script>
+
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+}
+</style>
