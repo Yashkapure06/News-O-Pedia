81edd200168181e50b43462e175878d2a79bc57f
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2023-03-03 01:22:29 +0530

    Added North America Continent Map

--- src/components/northamerica/NorthAmerica.vue
+++ src/components/northamerica/NorthAmerica.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1 class="main_heading">Welcome to North America</h1>
+    <img v-if="selectedRoute === '/northamerica/usa'" href="#" class="flag" width="270" height="150" src="https://rukminim1.flixcart.com/image/416/416/l0o6nbk0/poster/3/s/e/large-usa-flag-4k-on-hi-quality-large-print-36x24-inches-original-imagcenavhk97rw5.jpeg?q=70" alt="USA Flag">
+    <img v-if="selectedRoute === '/northamerica/canada'" href="#" class="flag" width="270" height="150" src="https://static.vecteezy.com/system/resources/previews/006/329/132/original/national-flag-of-canada-3d-rendering-waving-flag-high-quality-image-original-colors-sizes-and-shapes-free-photo.jpg" alt="Canada Flag">
+    
     <nav>
       <ul>
         <li>
@@ -13,13 +16,29 @@
       </ul>
     </nav>
     <router-view></router-view>
+    <p style="text-align:center">Currently Available Counties: <strong>The USA, Canada</strong></p>
+    <NorthAmericaMap/>
   </div>
 </template>
 
 <script>
+import NorthAmericaMap from './NorthAmericaMap.vue';
+
 export default {
-  name: 'NorthAmerica',
- 
+    name: "NorthAmerica",
+    components: { NorthAmericaMap },
+    
+    
+    data() {
+    return {
+      selectedRoute: ''
+    }
+  },
+  watch: {
+    '$route'(to) {
+      this.selectedRoute = to.path;
+    }
+  }
 }
 </script>
 
@@ -28,6 +47,14 @@ export default {
   text-align: center;
   margin-top: 50px;
   font-size: 100px;
+}
+
+.flag{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  max-width:270px;
+  max-height:150px;
 }
 nav{
   background-color: #333;
---
+++ src/components/northamerica/NorthAmericaMap.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 612 606">
+            <image width="612" height="606" xlink:href="../../assets/north-america.jpeg"></image> 
+            <router-link to="/northamerica/alaska">
+              <rect x="197" y="10" fill="#000" opacity="0" width="46" height="44"></rect>
+            </router-link>
+            <router-link to="/northamerica/canada">
+              <rect x="206" y="46" fill="#000" opacity="0" width="201" height="75"></rect>
+            </router-link>
+            <router-link to="/northamerica/usa">
+              <rect x="236" y="136" fill="#000" opacity="0" width="140" height="65"></rect>
+            </router-link>
+        </svg>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NorthAmericaMap'
+}
+</script>
+
+<style scoped>
+image{
+    width:100%;
+    height:50%;
+}
+</style>
